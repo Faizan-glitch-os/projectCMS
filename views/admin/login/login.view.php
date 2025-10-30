56a70cbec91e4a74e6b8c3e48111f214671607e0
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-10-30 13:07:12 +0500

    used alert in login form

--- views/admin/login/login.view.php
+++ views/admin/login/login.view.php
@@ -1,20 +1,26 @@
 <div class="container border border-1 p-5 my-4 rounded-4">
     <?php if (!empty($errors)): ?>
         <?php foreach ($errors as $error): ?>
-            <ul class="list-unstyled">
-                <li class="fs-8 text-danger"><?= $error ?></li>
-            </ul>
+            <div class="row">
+                <div class="alert alert-danger alert-dismissible fade show">
+                    <?= $error ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                </div>
+            </div>
         <?php endforeach ?>
     <?php endif ?>
-    <form action="index.php?<?= http_build_query(['route' => 'admin/login']) ?>" method="POST">
-        <div class="mb-3">
-            <label class="form-label text-black" for="email">Email Address</label>
-            <input class="form-control" type="text" name="email" id="email">
-        </div>
-        <div class="mb-3">
-            <label class="form-label text-black" for="password">Password</label>
-            <input class="form-control" type="password" name="password" id="password">
-        </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+    <div class="row">
+        <form class="form" action="index.php?<?= http_build_query(['route' => 'admin/login']) ?>" method="POST">
+            <div class="mb-3">
+                <label class="form-label text-black" for="email">Email Address</label>
+                <input class="form-control" type="text" name="email" id="email">
+            </div>
+            <div class="mb-3">
+                <label class="form-label text-black" for="password">Password</label>
+                <input class="form-control" type="password" name="password" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+
 </div>
